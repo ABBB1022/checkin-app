3bde68ed981a1f2c7978fcf3ec0251a7ca339633
ABBB1022 <136335582+ABBB1022@users.noreply.github.com>
2026-04-14 23:10:33 +0800

    Delete src/supabase.js

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://qifozesinprzjmbinomu.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpZm96ZXNpbnByemptYmlub211Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwMDA5NjAsImV4cCI6MjA5MTU3Njk2MH0.CeyN2v3E4neUF4ib-XrkKGg9WHLc1razl7ApUyh_L-8'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
