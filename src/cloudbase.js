35ca543abd064d1c26ff35240cd441403b0939ce
ABBB1022 <136335582+ABBB1022@users.noreply.github.com>
2026-04-15 22:32:35 +0800

    Update cloudbase.js

--- src/cloudbase.js
+++ src/cloudbase.js
@@ -4,8 +4,10 @@ const app = cloudbase.init({
   env: 'zzb-apps-3gxzi3br68a2bc66'
 })
 
-// 添加匿名登录
+// 认证模块
 export const auth = app.auth()
+
+// 数据库
 export const db = app.database()
 
 export const COLLECTIONS = {
@@ -13,7 +15,7 @@ export const COLLECTIONS = {
   RECORDS: 'checkin_records'
 }
 
-// 导出登录方法
+// 匿名登录
 export async function signInAnonymously() {
   await auth.signInAnonymously()
 }
